f2021e17ed17d1e7d0135116d07352721abee3d8
iamguiss <guizinhosilvasantos10@gmail.com>
2023-06-01 21:44:39 -0300

    gg

---
+++ app/src/main/java/br/com/etecia/merecicle/LocalizaActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.merecicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalizaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.localiza_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/merecicle/MaterialActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.merecicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaterialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_layout);
+    }
+}
